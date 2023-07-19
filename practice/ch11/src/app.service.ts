04a390aef12664482cfee0ca38eaff22a89ef891
Yu-Jaeyoung <20181650@o365.hanbat.ac.kr>
2023-07-19 22:59:27 +0900

    ch11 logger practice

---
+++ practice/ch11/src/app.service.ts
@@ -0,0 +1,19 @@
+import { Injectable, Logger } from '@nestjs/common';
+import { MyLogger } from './logging/my-logger.service';
+
+@Injectable()
+export class AppService {
+  constructor(private myLogger: MyLogger) { }
+
+  private readonly logger = new Logger(AppService.name);
+
+  getHello(): string {
+    this.myLogger.error('level: error');
+    this.myLogger.warn('level: warn');
+    this.myLogger.log('level: log');
+    this.myLogger.verbose('level: verbose');
+    this.myLogger.debug('level: debug');
+
+    return 'Hello World!';
+  }
+}
